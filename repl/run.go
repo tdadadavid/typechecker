5a1ddfa4ce417ec42d7057607a225fdad9555a14
tdadadavid <davidtofunmidada@gmail.com>
2026-01-06 19:00:51 +0100

    feat: add helper statements
    Signed-off-by: tdadadavid <davidtofunmidada@gmail.com>

--- repl/run.go
+++ repl/run.go
@@ -12,6 +12,8 @@ const PROMPT = ">>"
 func Run(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
+	printHelp(out)
+
 	for {
 		fmt.Fprint(out, PROMPT)
 		scanned := scanner.Scan()
@@ -38,4 +40,17 @@ func Run(in io.Reader, out io.Writer) {
 
 		fmt.Fprintf(out, "%+v\n", result)
 	}
+}
+
+func printHelp(out io.Writer) {
+	fmt.Fprintln(out, "Typechecker REPL")
+	fmt.Fprintln(out, "Examples:")
+	fmt.Fprintln(out, "  1")
+	fmt.Fprintln(out, "  'hello'")
+	fmt.Fprintln(out, "  \"hello\"")
+	fmt.Fprintln(out, "  true")
+	fmt.Fprintln(out, "  nil")
+	fmt.Fprintln(out, "  + 1 2")
+	fmt.Fprintln(out, "  (+ 1 2)")
+	fmt.Fprintln(out, "  [\"+\", 1, 2]")
 }
